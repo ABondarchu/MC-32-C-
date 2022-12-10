9aaa5e661ae9693164cb195a6e79ecaea9da4edb
Алексей бондарчук <76469391+ABondarchu@users.noreply.github.com>
2022-12-10 21:48:22 +0300

    Create LB_1.cs

---
+++ LB_1.cs
@@ -0,0 +1,31 @@
+using System;
+				
+public class MyClassA
+{
+	int a = 2;
+	int b = 4;
+	
+	public float C0
+	{
+		get { a *= b; return a; }
+	}
+	
+	
+	
+	public float C1
+	{
+		get { return a+b; }
+	}
+	
+}
+
+
+public class Program
+{
+	public static void Main()
+	{
+		MyClassA MyObject = new MyClassA();
+		Console.WriteLine(string.Format("C0 result: {0}\nC1 result: {1}\n", 
+										MyObject.C0, MyObject.C1));
+	}
+}
